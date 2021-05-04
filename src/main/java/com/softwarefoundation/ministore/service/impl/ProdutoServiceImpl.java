92f5224d09c703bdd025f2c817efd5960a7afc57
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-04 14:55:59 -0400

    9. Criando o Microserviço CRUD - ProdutoService

--- src/main/java/com/softwarefoundation/ministore/service/impl/ProdutoServiceImpl.java
+++ src/main/java/com/softwarefoundation/ministore/service/impl/ProdutoServiceImpl.java
@@ -1,10 +1,16 @@
 package com.softwarefoundation.ministore.service.impl;
 
 import com.softwarefoundation.ministore.dto.ProdutoDto;
+import com.softwarefoundation.ministore.entity.Produto;
+import com.softwarefoundation.ministore.exceptions.ResourceNotFoundException;
 import com.softwarefoundation.ministore.repository.ProdutoRepository;
 import com.softwarefoundation.ministore.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
@@ -16,7 +22,38 @@ public class ProdutoServiceImpl implements ProdutoService {
         this.produtoRepository = produtoRepository;
     }
 
-    public ProdutoDto create(ProdutoDto produtoDto){
-        return null;
+    public ProdutoDto create(ProdutoDto produtoDto) {
+        Produto pr = produtoRepository.save(Produto.create(produtoDto));
+        ProdutoDto dto = ProdutoDto.create(pr);
+        return dto;
     }
+
+    public Page<ProdutoDto> findAll(Pageable pageable) {
+        Page<Produto> page = produtoRepository.findAll(pageable);
+        return page.map(this::convertToProdutoDto);
+    }
+
+    private ProdutoDto convertToProdutoDto(Produto produto) {
+        return ProdutoDto.create(produto);
+    }
+
+    public ProdutoDto findById(Long id) {
+        var produto = produtoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return ProdutoDto.create(produto);
+    }
+
+
+    public ProdutoDto update(ProdutoDto produtoDto) {
+        final Optional<Produto> produtoOptional = produtoRepository.findById(produtoDto.getId());
+        if (!produtoOptional.isPresent()) {
+            new ResourceNotFoundException("No records found for this ID");
+        }
+        return ProdutoDto.create(produtoRepository.save(Produto.create(produtoDto)));
+    }
+
+    public void delete(Long id) {
+        var produto = produtoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        produtoRepository.delete(produto);
+    }
+
 }
