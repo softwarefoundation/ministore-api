8ba797006194fcb71e1b0891ddb6844e15021394
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-04 13:55:28 -0400

    Inicio projeto ministore-api

---
+++ src/main/java/com/softwarefoundation/ministore/MinistoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.ministore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundation/ministore/MinistoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.ministore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
